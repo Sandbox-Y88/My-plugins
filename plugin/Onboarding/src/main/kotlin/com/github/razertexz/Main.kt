4e82a3d6d3a74cfb2f9222a720c687ac33ebb46e
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-10-18 05:49:27 +0700

    Update Main.kt

--- plugin/Onboarding/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugin/Onboarding/src/main/kotlin/com/github/razertexz/Main.kt
@@ -13,24 +13,46 @@ import com.discord.stores.StoreStream
 
 private data class Onboarding(
     val guild_id: Long,
-    val prompts: List<Any>,
+    val prompts: List<OnboardingPrompt>,
     val default_channel_ids: List<Long>,
     val enabled: Boolean,
     val mode: Int
 )
 
+private class OnboardingPrompt(
+    val id: Long,
+    val type: Int,
+    val options: List<OnboardingPromptOption>,
+    val title: String,
+    val single_select: Boolean,
+    val required: Boolean,
+    val in_onboarding: Boolean
+)
+
+private class OnboardingPromptOption(
+    val id: Long,
+    val channel_ids: List<Long>,
+    val role_ids: List<Long>,
+    val emoji: Any?,
+    val emoji_id: Long?,
+    val emoji_name: String?
+    val emoji_animated: Boolean?,
+    val title: String,
+    val description: String?
+)
+
 @AliucordPlugin(requiresRestart = false)
 class Main : Plugin() {
     override fun start(ctx: Context) {
-        StoreStream.getGuildSelected().observeSelectedGuildId().subscribe {
-            val guildId = this
-
+        StoreStream.getGuildSelected().observeSelectedGuildId().subscribe(guildId -> {
             Utils.threadPool.execute {
                 val response = Http.Request.newDiscordRNRequest("https://discord.com/api/v9/guilds/$guildId/onboarding", "GET").execute()
                 val onboarding = response.json(Onboarding::class.java)
-                logger.warn(onboarding.toString())
+
+                if (onboarding.enabled)
+                    logger.warn(onboarding.toString())
             }
-        }
+        })
     }
 
     override fun stop(ctx: Context) = patcher.unpatchAll()
