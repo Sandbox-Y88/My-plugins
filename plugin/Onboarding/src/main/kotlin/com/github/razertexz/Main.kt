385a47ea0b53d07c56f4a71e278838e736449183
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-10-17 20:32:20 +0700

    Update Main.kt

--- plugin/Onboarding/src/main/kotlin/com/github/razertexz/Main.kt
+++ plugin/Onboarding/src/main/kotlin/com/github/razertexz/Main.kt
@@ -1,14 +1,40 @@
 package com.github.razertexz
 
 import android.content.Context
+import android.view.View
 
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
 import com.aliucord.patcher.*
+import com.aliucord.Http
+
+import com.discord.widgets.channels.list.items.ChannelListItemInvite
+import com.discord.widgets.channels.list.`WidgetChannelsListAdapter$ItemInvite$onConfigure$1`
+
+private data class Onboarding(
+    guild_id: Long,
+    prompts: List<Any>,
+    default_channel_ids: List<Long>,
+    enabled: Boolean,
+    mode: Any
+)
 
 @AliucordPlugin(requiresRestart = false)
 class Main : Plugin() {
     override fun start(ctx: Context) {
+        patcher.before<`WidgetChannelsListAdapter$ItemInvite$onConfigure$1`>("onClick", View::class.java) {
+            Utils.threadPool.execute {
+                val guildId = (`$data` as ChannelListItemInvite).guildId
+                val response = Http.Request.newDiscordRNRequest("https://discord.com/api/v9/guilds/${guildId}/onboarding", "GET").execute()
+
+                if (response.ok()) {
+                    val onboarding = response.json(Onboarding::class.java)
+                    logger.warn(onboarding)
+                }
+            }
+
+            it.result = null
+        }
     }
 
     override fun stop(ctx: Context) = patcher.unpatchAll()
