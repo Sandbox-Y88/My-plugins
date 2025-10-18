3d5358785c155f7bcd01e73a8ab002c893c93c7f
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-10-18 16:49:43 +0700

    Update OnboardingFragment.kt

--- plugin/Onboarding/src/main/kotlin/com/github/razertexz/OnboardingFragment.kt
+++ plugin/Onboarding/src/main/kotlin/com/github/razertexz/OnboardingFragment.kt
@@ -3,13 +3,14 @@ package com.github.razertexz
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.view.View
 import android.widget.FrameLayout
 
 import com.discord.app.AppFragment
 
 class OnboardingFragment() : AppFragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = FrameLayout(getContext()).apply {
+        val root = FrameLayout(requireContext()).apply {
             layoutParams = FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT)
         }
 
