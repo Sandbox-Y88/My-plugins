2ecf07dca0f997847d87a503489f2421ddff5604
RazerTexz <66409416+RazerTexz@users.noreply.github.com>
2025-10-18 15:52:28 +0700

    Update OnboardingFragment.kt

--- plugin/Onboarding/src/main/kotlin/com/github/razertexz/OnboardingFragment.kt
+++ plugin/Onboarding/src/main/kotlin/com/github/razertexz/OnboardingFragment.kt
@@ -2,14 +2,13 @@ package com.github.razertexz
 
 import android.view.View
 
-import com.aliucord.Utils
+import com.aliucord.Main
 
 import com.discord.app.AppFragment
 
 class OnboardingFragment() : AppFragment() {
     override fun onViewBound(view: View) {
         super.onViewBound(view)
-
-        Utils.showToast("${view.javaClass.name}")
+        Main.logger.info("${view.javaClass.name}"
     }
-}
+                         }
